80d60519ffc55c5e5e4b42c24ad48239f0d7a67c
gayemce <gayemce4@gmail.com>
2025-04-07 11:21:52 +0300

    feat: add Booking model and IMessageProducer interface

---
+++ FormulaAirline.API/Models/Booking.cs
@@ -0,0 +1,10 @@
+namespace FormulaAirline.API.Models;
+public class Booking
+{
+    public Guid Id { get; set; }
+    public string PassengerName { get; set; } = "";
+    public string PassportNb { get; set; } = "";
+    public string From { get; set; } = "";
+    public string To { get; set; } = "";
+    public int Status { get; set; }
+}
